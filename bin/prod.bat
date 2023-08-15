b8c43d51a5bc24c41a094fa1de183739bf29ae53
Aren Chilingaryan <aren.chilingaryan.dev@gmail.com>
2023-08-15 13:58:04 -0700

    refactor of deploy

--- bin/prod.bat
+++ bin/prod.bat
@@ -3,5 +3,17 @@ echo Stopping services on port 8080...
 FOR /F "tokens=5" %%T IN ('netstat -a -n -o ^| findstr :8080 ^| findstr LISTENING') DO (
     TASKKILL /F /PID %%T
 )
-echo Starting Minikube services
-minikube start
+@echo off
+
+FOR /F "tokens=* USEBACKQ" %%F IN (`minikube status --format "{{.Host}}"`) DO (
+   SET status=%%F
+)
+
+docker compose down
+
+IF "%status%" NEQ "Running" (
+    echo Starting Minikube...
+    minikube start
+) ELSE (
+    echo Minikube is already running.
+)
